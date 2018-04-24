87c0816a61e3b2602cbfe699ed42e03b73e7c33e
낙동강펭귄 <ych4870@gmail.com>
2018-04-24 21:42:55 +0900

    BoardQnaList 파일 수정

--- src/net/board/action/BoardQnaList.java
+++ src/net/board/action/BoardQnaList.java
@@ -7,6 +7,7 @@ public class BoardQnaList implements Action{
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("BoardQnaList execute()");
 		
 		
 		
