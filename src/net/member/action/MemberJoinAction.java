b143cdec9aefda7fb8e16c68de2d412e7d7fcaa0
Haenam Hwang <hairsnake@gmail.com>
2018-04-24 19:27:24 +0900

    MemberJoinAction.java 회원가입 액션페이지 추가

---
+++ src/net/member/action/MemberJoinAction.java
@@ -0,0 +1,44 @@
+package net.member.action;
+
+import java.sql.Timestamp;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.member.db.MemberDAO;
+import net.member.db.MemberDTO;
+
+public class MemberJoinAction implements Action{
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("MemberJoinAction execute()");
+	
+		request.setCharacterEncoding("UTF-8");
+		MemberDTO mDTO = new MemberDTO();
+		MemberDAO mDAO = new MemberDAO();
+		ActionForward forward = new ActionForward();
+		int result;
+
+		mDTO.setMember_id(request.getParameter("member_id"));
+		mDTO.setMember_pass(request.getParameter("member_pass"));
+		mDTO.setMember_name(request.getParameter("member_name"));
+		mDTO.setMember_post(request.getParameter("member_post"));
+		mDTO.setMember_address1(request.getParameter("member_address1"));
+		mDTO.setMember_address2(request.getParameter("member_address2"));
+		mDTO.setMember_phone(request.getParameter("member_phone"));
+		mDTO.setMember_email(request.getParameter("member_email"));
+		mDTO.setMember_date(new Timestamp(System.currentTimeMillis()));
+		
+		result = mDAO.insertjoin(mDTO);
+	
+		if(result==0){
+			System.out.println("Faile");
+		}else{
+			System.out.println("Sucsses");
+		}
+		
+		forward.setRedirect(true);
+		forward.setPath("MemberLogin.me");
+		return forward;
+	}
+}
