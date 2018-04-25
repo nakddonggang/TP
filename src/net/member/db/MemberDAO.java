c5713a89f857106f67de8392b33cd3f253b23e85
Haenam Hwang <hairsnake@gmail.com>
2018-04-25 22:29:33 +0900

    MemberDAO selectUserChk 유저체크 기능 추가

--- src/net/member/db/MemberDAO.java
+++ src/net/member/db/MemberDAO.java
@@ -24,12 +24,30 @@ public class MemberDAO {
 		return result;
 	}
 	
-	public List<MemberDTO> selectMember(String member_id) {
+	//맴버 유저체크 메소드
+	public int selectUserChk(String member_id, String member_pass) {
+		MemberDTO mDTO = new MemberDTO();
+		int check = 0;
+		mDTO = sqlsession.selectOne("selectMember", member_id);
+		
+		String id = mDTO.getMember_id();
+		
+		if (id != null) {	
+			if (mDTO.getMember_pass().equals(member_pass)) check = 1;
+			else check = 0;
+		} else check = -1;
+		
+		return check;
+	}
+	
+	//맴버 리스트 불러오기
+	public List<MemberDTO> selectMemberList(String member_id) {
 		List<MemberDTO> mList = new ArrayList<MemberDTO>();
 		mList = sqlsession.selectList("selectMember", member_id);
 		
 		return mList;
 	}
+	
 	
 	
 	
