4487e603cccf6626e0369679727031baada1db65
낙동강펭귄 <nakddonggang@pukyong.ac.kr>
2018-04-25 20:27:50 +0900

    BoardQnaUpdate 파일 생성

---
+++ src/net/board/action/BoardQnaUpdate.java
@@ -0,0 +1,22 @@
+package net.board.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BoardQnaUpdate implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("BoardQnaUpdate execute()");
+		request.setCharacterEncoding("utf-8");
+		
+		
+		ActionForward forward = new ActionForward();
+		
+		forward.setPath("./board/boardQnaUpdate.jsp");
+		forward.setRedirect(false);
+		
+		return forward;
+	}
+	
+}
