8d4c0f1cbab1e6031fc5ca4e2a82212773655637
yrkim <yrkim0312@naver.com>
2018-04-26 19:29:53 +0900

    BoardFaqWriteAction 수정

--- src/net/board/action/BoardFaqWriteAction.java
+++ src/net/board/action/BoardFaqWriteAction.java
@@ -3,6 +3,9 @@ package net.board.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.board.db.BoardDAO;
+import net.board.db.BoardDTO;
+
 public class BoardFaqWriteAction implements Action{
 
 	@Override
@@ -10,7 +13,23 @@ public class BoardFaqWriteAction implements Action{
 		System.out.println("BoardFaqWriteAction execute()");
 		request.setCharacterEncoding("utf-8");
 		
-		ActionForward forward = new ActionForward();
+		BoardDTO bDTO=new BoardDTO();
+		BoardDAO bDAO=new BoardDAO();
+		ActionForward forward=new ActionForward();
+		
+		
+		
+		bDTO.setFaq_num(Integer.parseInt(request.getParameter("faq_num")));
+		bDTO.setFaq_type(request.getParameter("faq_type"));
+		bDTO.setFaq_subject(request.getParameter("faq_subject"));
+		bDTO.setFaq_content(request.getParameter("faq_content"));
+		bDTO.setFaq_file(request.getParameter("faq_file"));
+		
+		
+		bDAO.insertFaq(bDTO);
+		
+	
+	
 		forward.setPath("./BoardFaqList.bo");
 		forward.setRedirect(true);
 		
