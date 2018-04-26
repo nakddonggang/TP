ba84a254b310ac7c5e26cde5eab6d8beae1a88ed
Haenam Hwang <hairsnake@gmail.com>
2018-04-26 18:57:46 +0900

    BoardDAO insertCur추가

--- src/net/board/db/BoardDAO.java
+++ src/net/board/db/BoardDAO.java
@@ -12,4 +12,15 @@ public class BoardDAO {
 	public BoardDAO(){
 		sqlsession = sessionf.openSession(true);
 	}
+	
+	public int insertCur(BoardDTO bDTO) {
+		int result;
+		int count = sqlsession.selectOne("countCur");
+		if((Integer)count == null ) count = -1;
+		System.out.println(count);
+		count += 1;
+		bDTO.setCur_num(count);
+		result = sqlsession.insert("insertCur", bDTO);
+		return result;
+	}
 }
