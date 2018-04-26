8f7514330b3cf9be950fdc49f766ad442bdfa599
yrkim <yrkim0312@naver.com>
2018-04-26 14:20:18 +0900

    BoardFaqWriteAction 수정

--- src/net/board/action/BoardFaqWriteAction.java
+++ src/net/board/action/BoardFaqWriteAction.java
@@ -1,5 +1,20 @@
 package net.board.action;
 
-public class BoardFaqWriteAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class BoardFaqWriteAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("BoardFaqWriteAction execute()");
+		request.setCharacterEncoding("utf-8");
+		
+		ActionForward forward = new ActionForward();
+		forward.setPath("./BoardFaqList.bo");
+		forward.setRedirect(true);
+		
+		return forward;
+	}
+  
 }
