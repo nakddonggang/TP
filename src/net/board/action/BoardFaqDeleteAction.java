663699fd1a687e1d000688475200919f1c9a63f2
yrkim <yrkim0312@naver.com>
2018-04-26 15:38:49 +0900

    BoardFaqDeleteAction 수정

--- src/net/board/action/BoardFaqDeleteAction.java
+++ src/net/board/action/BoardFaqDeleteAction.java
@@ -1,5 +1,16 @@
 package net.board.action;
 
-public class BoardFaqDeleteAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class BoardFaqDeleteAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("BoardFaqDeleteAction execute()");
+		request.setCharacterEncoding("utf-8");
+		
+		return null;
+	}
+   
 }
