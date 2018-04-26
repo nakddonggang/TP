4b8b55b6d5fadca5387e8e3e83590de1019df0d2
낙동강펭귄 <nakddonggang@pukyong.ac.kr>
2018-04-26 20:10:16 +0900

    Qna 컨트롤러 생성

---
+++ src/net/board/action/QnaFrontController.java
@@ -0,0 +1,83 @@
+package net.board.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class QnaFrontController extends HttpServlet{
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 가상주소 뽑아오기
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestURI.substring(contextPath.length());
+		System.out.println("뽑아온 가상주소 : "+command);
+		
+		ActionForward forward = null;
+		Action action = null;
+		
+		if(command.equals("/BoardQnaList.bo")){
+			action = new BoardQnaList();
+			try{
+				forward = action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		} else if(command.equals("/BoardQnaWrite.bo")){
+			forward = new ActionForward();
+			forward.setPath("./board/boardQWrite.jsp");
+			forward.setRedirect(false);
+		} else if(command.equals("/BoardQnaWriteAction.bo")){
+			action = new BoardQnaWriteAction();
+			try{
+				forward = action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		} else if(command.equals("/BoardQnaContent.bo")){
+			action = new BoardQnaContent();
+			try{
+				forward = action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		} else if(command.equals("/BoardQnaSearch.bo")){
+			action = new BoardQnaSearch();
+			try{
+				forward = action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		} else if(command.equals("/BoardQnaDeleteAction.bo")){
+			action = new BoardQnaDeleteAction();
+			try{
+				forward = action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		if(forward != null){
+			if(forward.isRedirect){
+				response.sendRedirect(forward.getPath());
+			}else{
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+}
