52d52b6aa4756916f149b5398a9fe895f72fc20f
yrkim <yrkim0312@naver.com>
2018-04-26 15:39:38 +0900

    BoardFaqUpdateAction 수정

--- src/net/board/action/BoardFaqUpdateAction.java
+++ src/net/board/action/BoardFaqUpdateAction.java
@@ -1,5 +1,16 @@
 package net.board.action;
 
-public class BoardFaqUpdateAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class BoardFaqUpdateAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("BoardFaqUpdateAction execute()");
+		request.setCharacterEncoding("utf-8");
+		
+		return null;
+	}
+	
 }
