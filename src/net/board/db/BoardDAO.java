eba3e806355177020904e24ac47cf9d97938dd76
낙동강펭귄 <nakddonggang@pukyong.ac.kr>
2018-04-26 18:11:44 +0900

    BoardDAO에 insertNotice, selectMaxNum 메서드 추가

--- src/net/board/db/BoardDAO.java
+++ src/net/board/db/BoardDAO.java
@@ -1,8 +1,14 @@
 package net.board.db;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Vector;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
+import net.member.db.MemberDTO;
 import util.myBatisSetting.sqlMapConfig;
 
 public class BoardDAO {
@@ -12,4 +18,32 @@ public class BoardDAO {
 	public BoardDAO(){
 		sqlsession = sessionf.openSession(true);
 	}
+	
+	//NOtice 게시판 글쓰기
+	public int insertNotice(BoardDTO bDTO){
+		int result;
+		result = sqlsession.insert("insertNotice", bDTO);
+		return result;
+	}
+	
+	
+	//Qna 게시판 글쓰기
+	public int insertQna(Vector vector) {
+		int result;
+		HashMap map = new HashMap<>();
+		List<BoardDTO> list = (List<BoardDTO>)vector.get(0);
+		String id = (String)vector.get(1);
+		map.put("list", list);
+		map.put("id", id);
+		
+		result = sqlsession.insert("insertQna", map);
+		return result;
+	}
+	
+	public int selectMaxNum() {
+		int maxNum;
+		maxNum = sqlsession.selectOne("selectMaxNum");
+		return maxNum;
+	}
+	
 }
