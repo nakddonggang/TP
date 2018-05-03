b88f3d64198314692e93f681fd16baa2aa6833b1
yrkim <yrkim0312@naver.com>
2018-05-03 14:35:40 +0900

    Book관련    콘트롤러, web.xml 추가

---
+++ src/net/book/action/BookFrontController.java
@@ -0,0 +1,63 @@
+package net.book.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.actionForward.Action;
+import util.actionForward.ActionForward;
+
+public class BookFrontController extends HttpServlet{
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 가상주소 뽑아오기
+				String requestURI = request.getRequestURI();
+				System.out.println("requestURI주소 : "+requestURI);
+
+				String contextPath = request.getContextPath();
+				System.out.println("contextPath 경로 : "+contextPath);
+				System.out.println("contextPath 경로 길기 : "+contextPath.length());
+
+				String command = requestURI.substring(contextPath.length());
+				System.out.println("command - 뽑아온 가상주소 경로 : "+command);
+				
+				// 뽑아온 주소 비교
+				ActionForward forward = null;
+				Action action = null;
+				
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		if (forward!=null){
+			if (forward.isRedirect())
+				response.sendRedirect(forward.getPath());
+			else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}		
+	}
+	
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+}
