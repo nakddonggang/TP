affc68c3668faf132ff43cfb452a899652aac011
jinseok <ssama93@naver.com>
2018-05-08 18:38:48 +0900

    Date형 타입 변경 java.sql.Date -> java.util.Date

--- src/net/main/action/MainFrontController.java
+++ src/net/main/action/MainFrontController.java
@@ -25,10 +25,13 @@ public class MainFrontController extends HttpServlet{
 		ActionForward forward = null;
 		Action action = null;
 		
-		if(command.equals("/Main.fp")){
-			forward = new ActionForward();
-			forward.setPath("main/main.jsp");
-			forward.setRedirect(false);
+		if(command.equals("/Main.fp")) {
+			action = new MainAction();
+		try {
+			forward = action.execute(request, response);		
+			} catch (Exception e) {
+			e.printStackTrace();
+		}
 		}if(command.equals("/GuideMap.fp")){
 			forward = new ActionForward();
 			forward.setPath("guide/guideMap.jsp");
