42de6f7b6dae9398085472adf08bd3812b6790d7
jinseok <ssama93@naver.com>
2018-05-15 12:10:24 +0900

    sugg java 추가

---
+++ src/net/admin/action/AdminFacilSugg.java
@@ -0,0 +1,18 @@
+package net.admin.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.actionForward.Action;
+import util.actionForward.ActionForward;
+
+public class AdminFacilSugg implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+	
+}
