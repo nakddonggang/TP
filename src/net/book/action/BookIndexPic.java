9fbd1e2edfb80b9fa700c6bfa46d019dae709492
youngji <muse_cherish@naver.com>
2018-05-18 19:18:32 +0900

    admin 커밋

--- src/net/book/action/BookIndexPic.java
+++ src/net/book/action/BookIndexPic.java
@@ -1,4 +1,4 @@
-package net.admin.action;
+package net.book.action;
 
 import java.io.PrintWriter;
 import java.util.List;
@@ -6,24 +6,23 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.admin.db.AdminDAO;
+import net.book.db.BookDAO;
 import net.book.db.BookDTO;
 import util.actionForward.Action;
 import util.actionForward.ActionForward;
 
-public class AdminBookRes implements Action{
-
+public class BookIndexPic  implements Action{
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// 한글처리
 		request.setCharacterEncoding("UTF-8");
 		
 		// AdminDAO 객체생성 및 total 책의 개수 가져오기
-		AdminDAO adao = new AdminDAO();
-		int count = adao.getRBookCount();
+		BookDAO bdao = new BookDAO();
+		int count = bdao.BookCount();
 		
 		// 한 화면에 보여줄 책의 개수 설정
-		int pageSize = 5;		
+		int pageSize = 12;
 		
 		// 페이지 번호 (PageNum)
 		String pageNum = request.getParameter("pageNum");
@@ -36,22 +35,20 @@ public class AdminBookRes implements Action{
 		// 첫 번째 페이지 첫행 구하기
 			// 1page - 1 / 2page - 11 / 3page - 21
 		int startRow = (currentPage-1)* pageSize+1;
-		System.out.println(startRow);
 		// 페이지 마지막행 구하기
 			// 1page - 10 / 2page - 20 / 3page - 30
 		int endRow = pageSize*currentPage;
-		System.out.println(endRow);
 		
 		// 입출력
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
 		// 책 뿌려주는 메소드 생성
-		List<BookDTO> rbookList = null;
+		List<BookDTO> bookList = null;
 		if (count!=0) {
-			rbookList=adao.getRBookList(startRow, pageSize);
+			bookList=bdao.BookList(startRow, pageSize);
 		} else {
-			
+			System.out.println("책 목록이 없습니다");
 		}
 
 		// 게시판 전체 페이지 수
@@ -70,16 +67,16 @@ public class AdminBookRes implements Action{
 		// count, pageNum, boardList, pageCount, pageBlock, startPage, endPage 저장
 		request.setAttribute("count", count);
 		request.setAttribute("pageNum", pageNum);
-		request.setAttribute("rbookList", rbookList);
+		request.setAttribute("bookList", bookList);
 		request.setAttribute("pageCount", pageCount);
 		request.setAttribute("pageBlock", pageBlock);
 		request.setAttribute("startPage", startPage);
 		request.setAttribute("endPage", endPage);		
 		
 		ActionForward forward = new ActionForward();
-		forward.setPath("./admin/admBookRes.jsp");
+		forward.setPath("./book/BookIndexPic.jsp");
 		forward.setRedirect(false);
 		return forward;
+		
 	}
-	
 }
