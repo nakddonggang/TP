04b83145af874ba2163e3ffb3782e7a76dca3e4d
낙동강펭귄 <nakddonggang@pukyong.ac.kr>
2018-05-21 11:53:12 +0900

    예약 기능 수정

--- src/net/member/action/AcceptResBook.java
+++ src/net/member/action/AcceptResBook.java
@@ -21,15 +21,17 @@ public class AcceptResBook implements Action{
 		HttpSession session = request.getSession();
 		String member_id = (String)session.getAttribute("member_id");
 		
-		int book_number = 3;
+		int book_number = 2;
 		
 		MemberDAO mDAO = new MemberDAO();
 		BookDTO bDTO = new BookDTO();
-		bDTO.setBook_number(book_number);
 		bDTO.setMember_id(member_id);
+		bDTO.setBook_number(book_number);
 		mDAO.insertBbook(bDTO);				// 대출테이블에 insert
 		mDAO.updateRbook(book_number);		// 예약대기순위 1 감소
 		mDAO.deleteRbook();					// 예약대기순위 0인것 예약테이블에서 삭제
+		mDAO.updateRbookCheck();
+		mDAO.updateRbookCheck2();
 		
 		forward.setPath("./MemberBasketList.me");
 		forward.setRedirect(true);
